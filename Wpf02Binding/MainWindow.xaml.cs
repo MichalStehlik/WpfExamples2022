1ecc94c38cb21b5bef83651167824733d9ecc05d
Michal Stehlík <michal.stehlik@pslib.cz>
2022-03-02 08:43:37 +0100

    wpf02 first example

---
+++ Wpf02Binding/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf02Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // 1. zpusob
+        private void txtInput1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox)
+            {
+                var text = (sender as TextBox).Text;
+                lblResult1.Content = text;
+            }
+        }
+
+        private void sliSize1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (sender is Slider)
+            {
+                var size = (sender as Slider).Value;
+                lblResult1.FontSize = size;
+            }
+        }
+    }
+}
