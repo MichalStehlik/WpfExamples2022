d7adc64e3fb7218e831f5c8ca2e5d7b7be808252
Michal Stehlík <michal.stehlik@pslib.cz>
2022-03-09 08:45:35 +0100

    wpf03converters

---
+++ Wpf03Convertors/Convertors/NumberToTextConvertor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Wpf03Convertors.Convertors
+{
+    internal class NumberToTextConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int? /*&& targetType == typeof(string)*/)
+            {
+                switch (value as int?)
+                {
+                    case 0: return "Nula";
+                    case 1: return "Jedna";
+                    case 2: return "Dva";
+                    case 3: return "Tři";
+                    case 4: return "Čtyři";
+                    default: return "Moc";
+                }
+            }
+            return value.ToString();
+            // return null;
+            // return default;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Wpf03Convertors/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf03Convertors.ViewModels
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private int _number;
+        private string _text;
+
+        public MainViewModel()
+        {
+            Number = 2;
+        }
+
+        public int Number 
+        { 
+            get 
+            { 
+                return _number; 
+            } 
+            set 
+            { 
+                _number = value;
+                switch (_number)
+                {
+                    case 0: Text = "Nula"; break;
+                    case 1: Text = "Jedna"; break;
+                    case 2: Text = "Dva"; break;
+                    case 3: Text = "Tři"; break;
+                    case 4: Text = "Čtyři"; break;
+                    default: Text = "Moc"; break;
+                }
+                NotifyPropertyChanged();
+                NotifyPropertyChanged("Text2");
+            } 
+        }
+        public string Text { get { return _text; } set { _text = value; NotifyPropertyChanged(); } }
+        public string Text2 { get { return "XXXXX" + _number.ToString(); }  }
+    }
+}
