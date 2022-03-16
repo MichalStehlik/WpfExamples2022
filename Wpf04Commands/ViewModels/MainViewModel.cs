cb5d003937bd46829ee9b08fa304dec83166918b
Michal Stehlík <michal.stehlik@pslib.cz>
2022-03-16 08:44:50 +0100

    commands start

---
+++ Wpf04Commands/ViewModels/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf04Commands.ViewModels
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Random _random = new Random();
+        private int _number;
+
+        public RelayCommand RollCommand { get; set; }
+
+        public MainViewModel()
+        {
+            RollCommand = new RelayCommand(
+                () =>
+                {
+                    Number = _random.Next(100);
+                },
+                () => { return true; }
+                );
+            Number = _random.Next(100);
+        }
+
+        public int Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                _number = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
